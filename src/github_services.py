a279615975b0346b83b926f438cb98555acea5d4
Kevin Thomas <kevintab@tutanota.com>
2025-02-01 01:37:59 -0800

    update condition

--- src/github_services.py
+++ src/github_services.py
@@ -199,6 +199,8 @@ def get_pull_request_dict_with_timestamp(
     """
 
     for assignee in pr_dict['assignees']:
+        if event['assignee'] is None or assignee is None:
+            continue
         if event['assignee']['login'] == assignee['login']:
             assignee['created_at'] = parser.parse(event['created_at'])
     return pr_dict
